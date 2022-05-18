f424afb971297b64c41455f97c7fa9774ed880b5
teamlead <teamlead@gmail.com>
2022-05-18 18:49:52 +0530

    committing the code added on interface in main class

--- springbootjpainitegration/src/main/java/com/skilllogic/sspringbootjpainitegration/SpringbootjpainitegrationApplication.java
+++ springbootjpainitegration/src/main/java/com/skilllogic/sspringbootjpainitegration/SpringbootjpainitegrationApplication.java
@@ -1,10 +1,14 @@
 package com.skilllogic.sspringbootjpainitegration;
 
+import java.io.Serializable;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootjpainitegrationApplication {
+public class SpringbootjpainitegrationApplication implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjpainitegrationApplication.class, args);
